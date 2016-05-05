af70036ecfd80319e48e9f883edf109fb4d9c8cd
Bernhard Posselt <dev@bernhard-posselt.com>
2016-05-05 10:55:52 +0200

    Add aliasing and sharing to container

--- owncloud_news_updater/dependencyinjection/container.py
+++ owncloud_news_updater/dependencyinjection/container.py
@@ -6,6 +6,27 @@ class AlreadyRegisteredException(Exception):
     pass
 
 
+class Factory:
+    """
+    Wrapper for non shared factories
+    """
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def __call__(self, *args, **kwargs):
+        return self.factory(*args, **kwargs)
+
+
+class SingletonFactory(Factory):
+    """
+    Wrapper for shared factories
+    """
+
+    def __init__(self, factory):
+        super().__init__(factory)
+
+
 class BaseContainer:
     """
     Simple container for Dependency Injection
@@ -15,19 +36,26 @@ class BaseContainer:
         self._singletons = {}
         self._factories = {}
 
-    def register(self, key, factory):
+    def register(self, key, factory, shared=True):
         """
         Registers a factory function for creating the class
         :argument key name under which the dependencies will be
         registered
         :argument factory a function that receives the constructor as first
         argument and is used to create the class
+        :argument shared if True, the factory result will be saved for reuse.
+        This means that the same instance will be returned once the same key
+        is requested again. If False requesting the same key will always return
+        a new instance. Autoresolved classes will always be shared
         """
         if key in self._factories:
             msg = 'Factory already registered for key %s' % key
             raise AlreadyRegisteredException(msg)
         else:
-            self._factories[key] = factory
+            if shared:
+                self._factories[key] = SingletonFactory(factory)
+            else:
+                self._factories[key] = Factory(factory)
 
     def resolve(self, key):
         """
@@ -36,16 +64,34 @@ class BaseContainer:
         :return the created or looked up instance
         """
         if key not in self._singletons:
+            # if registered, determine if the result will be shared
             if key in self._factories:
-                self._singletons[key] = self._factories[key](self)
+                factory = self._factories[key]
+                result = factory(self)
+                if isinstance(factory, SingletonFactory):
+                    self._singletons[key] = result
+                return result
+            # if not registered, try to automatically resolve it based on
+            # its annotations. Automatically resolved keys are always shared
             elif isinstance(type(key), type):
-                self._singletons[key] = self._create_class(key)
+                result = self._resolve_class(key)
+                self._singletons[key] = result
+                return result
             else:
                 msg = 'Unable to resolve factory for key %s' % key
                 raise ResolveException(msg)
-        return self._singletons[key]
+        else:
+            return self._singletons[key]
 
-    def _create_class(self, clazz):
+    def alias(self, source, target):
+        """
+        Point a key to another key
+        :argument source the key to resolve when the target is requested
+        :argument target the name of the alias
+        """
+        self.register(target, lambda: self.resolve(source))
+
+    def _resolve_class(self, clazz):
         """
         Constructs an instance based on the function annotations on an object's
         __init__ method
