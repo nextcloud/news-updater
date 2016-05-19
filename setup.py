a1203c98756c02b0ce81ff5af766a4a4a7e7d073
Bernhard Posselt <dev@bernhard-posselt.com>
2016-05-20 00:43:54 +0200

    test typings

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ if version_info < (3, 4):
     print('Error: Python 3.4 required but found %s' % python_version())
     exit(1)
 
+if version_info < (3, 5):
+    install_requires = ['typing']
+else:
+    install_requires = []
+
 with open('README.rst', 'r') as infile:
     long_description = infile.read()
 
@@ -24,8 +29,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license='GPL',
-    install_requires=[],
     keywords=['nextcloud', 'news', 'updater', 'RSS', 'Atom', 'feed', 'reader'],
+    install_requires=install_requires,
     classifiers=[
         'Intended Audience :: System Administrators',
         'Environment :: Console',
